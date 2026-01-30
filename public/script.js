352d5456c8a775a87e91e35d96ec3989b03b5376
ADIFF-14 <adiff0414@gmail.com>
2026-01-29 14:56:44 -1200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,44 @@
+const socket = io();
+let esAdmin = false;
+
+socket.on('vistas', (tipo) => {
+    if (tipo === 'PROYECTOR') window.location.href = "proyector.html";
+
+    if (tipo === 'ADMIN') {
+        esAdmin = true;
+        document.getElementById("admin-panel").style.display = "block";
+        document.getElementById("adminBtn").style.display = "block";
+    }
+    showScreen("waiting");
+});
+
+function entrarJuego() {
+    const n = document.getElementById("userName").value.trim();
+    if (n) socket.emit('unirse', { nombre: n });
+}
+
+socket.on('infoSecretaAdmin', (data) => {
+    if (esAdmin) {
+        const imp = data.jugadores.find(j => j.rol === "IMPOSTOR");
+        document.getElementById("admin-info").innerText =
+          `IMPOSTOR: ${imp.nombre} | PALABRA: ${data.palabra}`;
+
+        document.getElementById("btn-debate-fijo").style.display = "block";
+    }
+});
+
+socket.on('recibirRol', (data) => {
+    if (data.rol === "IMPOSTOR") {
+        document.getElementById("roleTitle").innerText = "ERES EL IMPOSTOR";
+        document.getElementById("roleText").innerText =
+          "Descubre la frase secreta de los demás";
+    } else {
+        document.getElementById("roleTitle").innerText = "TU FRASE ES:";
+        document.getElementById("roleText").innerText = data.palabra;
+    }
+    showScreen("role");
+});
+
 socket.on('cambioDeTurno', (data) => {
     if (esAdmin) document.getElementById("btn-debate-fijo").style.display = "none";
 
@@ -54,6 +95,7 @@ function showScreen(id) {
 socket.on('actualizarLista', (n) => {
     document.getElementById("count").innerText = n;
 });
+
 
 
 
